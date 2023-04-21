7e8240c0e650f2aecac65f3b904da449132527f0
Mirna <ampueromirna8@gmail.com>
2023-04-21 14:42:58 -0500

    fixed linters

--- student_school.rb
+++ student_school.rb
@@ -1,6 +1,6 @@
 class Student < Person
   attr_accessor :classroom
-  
+
   def initialize(age, parent_permission: true, name: 'Unknown', classroom: nil)
     super(age, parent_permission, name)
     self.classroom = classroom if classroom
